ee8028e5f766d493f68efda300eafc03c7198e0e
Cameron Perot <cameronperot@users.noreply.github.com>
2020-11-11 14:34:59 +0100

    Added script for miniconda2 install

--- python-environment/miniconda2-setup.sh
+++ python-environment/miniconda2-setup.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Setting up the miniconda directory
-install_dir=/opt/miniconda3
-conda_url=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+install_dir=/opt/miniconda2
+conda_url=https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh
 sudo mkdir $install_dir
 sudo chown $USER:$USER $install_dir
 
@@ -11,7 +11,3 @@ sudo chown $USER:$USER $install_dir
 cd $install_dir
 sudo wget -O miniconda.sh $conda_url
 bash miniconda.sh -b -f -p $install_dir
-
-# Update and install packages
-export PATH=$install_dir/bin:$PATH
-conda update -y conda
