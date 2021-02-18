2d5af43ac7614e74b199bfd8366881a3cd1ad1df
Cameron Perot <cameronperot@users.noreply.github.com>
2021-02-18 19:10:56 +0100

    Updated julia scripts

--- julia-environment/julia-setup.sh
+++ julia-environment/julia-setup.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 set -eu -o pipefail
 
-dir=$PWD
-install_dir=/opt/miniconda3/envs/jupyter
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P)"
+
+install_dir=$HOME
+rm -rf $install_dir
+mkdir $install_dir
+
 version_major=1
-version_minor=4
-version_patch=1
-
+version_minor=5
+version_patch=3
 version=$version_major.$version_minor.$version_patch
-url=https://julialang-s3.julialang.org/bin/linux/x64/$version_major.$version_minor/julia-$version-linux-x86_64.tar.gz
-file=julia-$version-linux-x86_64.tar.gz
 
-cd $install_dir
+url=https://julialang-s3.julialang.org/bin/linux/x64/$version_major.$version_minor/julia-$version-linux-x86_64.tar.gz
+file=/tmp/julia-$version-linux-x86_64.tar.gz
+
 wget -O $file $url
-tar -xf $file
+tar --directory $install_dir -xf $file
 
 sudo dnf install hdf5-devel
-$install_dir/julia-$version/bin/julia $dir/julia-setup.jl
+$install_dir/julia-$version/bin/julia $DIR/julia-setup.jl
